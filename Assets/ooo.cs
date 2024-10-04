9c4c4956659aba2d2ea81562f2e0caff03a3170d
oregobu <ryoga11.17@icloud.com>
2024-10-04 10:35:04 +0900

    Update ooo.cs

--- Assets/ooo.cs
+++ Assets/ooo.cs
@@ -7,7 +7,7 @@ public class ooo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //push用テストコメント
     }
 
     // Update is called once per frame
