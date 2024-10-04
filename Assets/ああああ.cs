eac5d3e5818d40d3e1c4b7fa11cf6c39e96ac52d
nekomochi4 <nekomochi.game@gmail.com>
2024-10-04 09:20:38 +0900

    Initial commit

---
+++ Assets/ああああ.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ああああ : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //テストです  更新確認
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //こーそ
+    }
+}
